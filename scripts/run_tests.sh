091e91c049b93778d72700e8e8d88fa7060f26c3
cnouguier <ch.nouguier@gmail.com>
2026-04-10 08:48:14 +0200

    ci: added scripts

---
+++ scripts/run_tests.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -x
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "$THIS_FILE")
+ROOT_DIR=$(dirname "$THIS_DIR")
+WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+
+. "$THIS_DIR/kash/kash.sh"
+
+## Parse options
+##
+
+NODE_VER=20
+MONGO_VER=""
+CI_STEP_NAME="Run tests"
+RUN_SONAR=false
+while getopts "n:sr:" option; do
+    case $option in
+        n) # defines node version
+            NODE_VER=$OPTARG
+             ;;
+        s) # publish code coverage
+            RUN_SONAR=true
+            ;;
+        r) # report outcome to slack
+            CI_STEP_NAME=$OPTARG
+            load_env_files "$WORKSPACE_DIR/development/common/SLACK_WEBHOOK_LIBS.enc.env"
+            trap 'slack_ci_report "$ROOT_DIR" "$CI_STEP_NAME" "$?" "$SLACK_WEBHOOK_LIBS"' EXIT
+            ;;
+        *)
+            ;;
+    esac
+done
+
+## Init workspace
+##
+
+. "$WORKSPACE_DIR/development/workspaces/services/services.sh" services-ekosystem
+
+## Run tests
+##
+
+run_lib_tests "$ROOT_DIR" "$RUN_SONAR" "$NODE_VER" "$MONGO_VER"
---
+++ scripts/setup_workspace.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -x
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "$THIS_FILE")
+ROOT_DIR=$(dirname "$THIS_DIR")
+WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+
+. "$THIS_DIR/kash/kash.sh"
+
+## Parse options
+##
+
+begin_group "Setting up workspace ..."
+
+if [ "$CI" != true ]; then
+    while getopts "b:t" option; do
+        case $option in
+            b) # defines branch
+                WORKSPACE_BRANCH=$OPTARG;;
+            t) # defines tag
+                WORKSPACE_TAG=$OPTARG;;
+            *)
+            ;;
+        esac
+    done
+
+    shift $((OPTIND-1))
+    WORKSPACE_DIR="$1"
+
+    # Clone project in the workspace
+    git_shallow_clone "$KALISIO_GITHUB_URL/kalisio/services-ekosystem.git" "$WORKSPACE_DIR/services-ekosystem" "${WORKSPACE_TAG:-${WORKSPACE_BRANCH:-}}"
+fi
+
+setup_lib_workspace "$WORKSPACE_DIR" "$KALISIO_GITHUB_URL/kalisio/development.git"
+
+end_group "Setting up workspace ..."
