2ab90fa1a1597b880d3898b4d4e2111aaba20503
Devneb01 <chahbouniali37@gmail.com>
2026-05-28 18:51:31 +0200

    refactor: update layers loading logic and improve service structure

--- packages/katalog/src/layers.js
+++ packages/katalog/src/layers.js
@@ -1,13 +1,8 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { globSync } from 'glob'
-import { kdkMapApiLayersLoader, kdkMapApiCategoriesLoader, kdkMapApiSublegendsLoader } from '@kalisio/kdk-map-api'
-
+import { config } from '@kalisio/kdk-map-api'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-const getLayers = kdkMapApiLayersLoader
-const getCategories = kdkMapApiCategoriesLoader
-const getSublegends = kdkMapApiSublegendsLoader
 
 const kargoDomain = (process.env.SUBDOMAIN ? process.env.SUBDOMAIN : 'test.kalisio.xyz')
 const wmtsUrl = (process.env.API_GATEWAY_URL ? process.env.API_GATEWAY_URL + '/wmts/1.0.0' : 'https://mapcache.' + kargoDomain + '/mapcache/wmts/1.0.0')
@@ -35,7 +30,7 @@ export async function loadLayers (app) {
 
   const context = Object.assign({ wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }, app.get('catalog') || {})
 
-  const layers = getLayers(layerFiles, context)
+  const layers = config.loadLayers(layerFiles, context)
 
   return layers
 }
@@ -44,11 +39,11 @@ export async function loadCategories (app) {
   const categoriesDir = path.resolve(__dirname, '../config/categories')
   const categoryFiles = globSync(path.join(categoriesDir, '**/*.cjs').replace(/\\/g, '/'))
   const context = Object.assign({ domain: kargoDomain, wmtsUrl, tmsUrl, wmsUrl, wcsUrl, k2Url, s3Url }, app.get('catalog') || {})
-  return getCategories(categoryFiles, context)
+  return config.loadCategories(categoryFiles, context)
 }
 
 export async function loadSublegends (app) {
   const sublegendsDir = path.resolve(__dirname, '../config/sublegends')
   const sublegendFiles = globSync(path.join(sublegendsDir, '**/*.cjs').replace(/\\/g, '/'))
-  return getSublegends(sublegendFiles)
+  return config.loadSublegends(sublegendFiles)
 }
